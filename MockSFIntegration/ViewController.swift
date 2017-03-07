bdc478c682ccaaa62db762a1cc6f59b6ff16dc28
Shad Downey <shad@1stdibs.com>
2017-03-07 12:06:55 -0500

    Initial Commit

---
+++ MockSFIntegration/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MockSFIntegration
+//
+//  Created by Shad Downey on 3/7/17.
+//  Copyright © 2017 Shad Downey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
